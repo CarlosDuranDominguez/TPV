9e70934e60ef3ffd349ca4936af516bba67ada69
kyraNET <kyradiscord@gmail.com>
2018-12-04 09:09:28 +0100

    src: Added error classes

---
+++ ProyectosSDL/HolaSDL/ArkanoidError.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <exception>
+#include <stdexcept>
+using namespace std;
+
+class ArkanoidError : public logic_error {
+public:
+    ArkanoidError(const string& m): logic_error("Arkanoid Error " + m) {}
+};
---
+++ ProyectosSDL/HolaSDL/FileNotFoundError.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ArkanoidError.h"
+#include <SDL.h>
+
+
+class FileNotFoundError : public ArkanoidError {
+public:
+    FileNotFoundError(const string& name) : ArkanoidError("[File Not Found]: " + name) {}
+};
